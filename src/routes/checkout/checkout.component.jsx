2b3249fcf157f7cd1aedd78c34fee57143a64787
Galuh Ramadhan <galuh.html@gmail.com>
2022-12-25 18:19:24 +0700

    stripe integration

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,13 +3,14 @@ import { useSelector } from 'react-redux';
 import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import PaymentForm from '../../components/payment-form/payment-form.component';
 
 import './checkout.styles.scss';
 
 const Checkout = () => {
     const cartItems = useSelector(selectCartItems);
     const cartTotal = useSelector(selectCartTotal);
-    
+
 
     return (
         <div className="checkout-container">
@@ -34,6 +35,7 @@ const Checkout = () => {
                 cartItems.map((cartItem) => (<CheckoutItem key={cartItem.id} cartItem={cartItem} />))
             }
             <span className="total">Total: ${cartTotal}</span>
+            <PaymentForm />
         </div>
     );
 }
